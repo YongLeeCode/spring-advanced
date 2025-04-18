b03b9d5a91fbc42fd4a0434d2df1f205ffdd7fb9
YongLeeCode <yonghwan311@gmail.com>
2025-04-18 18:05:06 +0900

    refactor: valid 어노테이션 적용

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,9 @@
 package org.example.expert.domain.user.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +16,8 @@ public class UserChangePasswordRequest {
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상")
+    @Pattern(regexp = ".*\\d.*")
+    @Pattern(regexp = ".*[A-Z].*")
     private String newPassword;
 }
